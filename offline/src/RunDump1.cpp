c7c00da1ded19c6b1880e450163253bbaee1d817
pdauncey <daq@hgcbeamtestpc.dyndns.cern.ch>
2023-07-28 14:06:57 +0200

    PC

--- offline/src/RunDump1.cpp
+++ offline/src/RunDump1.cpp
@@ -69,7 +69,7 @@ int main(int argc, char** argv) {
 
 	// We have an event record
       nEvents++;
-      bool print(nEvents<=100);// || nEvents>2000);
+      bool print(nEvents<=4000);// || nEvents>2000);
       //anyError=false;
 
       // Check id is correct
