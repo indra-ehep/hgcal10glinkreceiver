d54822b9525fd51a8a9bada073c43ded58dddf20
pdauncey <raghunandan.atul.shukla@cern.ch>
2023-09-11 16:25:26 +0200

    PC

--- offline/src/RunDump0.cpp
+++ offline/src/RunDump0.cpp
@@ -1,8 +1,62 @@
 #include <iostream>
 #include <iomanip>
 #include <cassert>
+#include <vector>
 
 #include "FileReader.h"
+
+void econtEnergies(const uint64_t *p, std::vector<uint16_t> &v) {
+  v.resize(24);
+
+  uint64_t e0,e1;
+  
+  e0=p[0]<<32|p[1]&0xffffffff;
+  e1=p[1]<<48|(p[2]&0xffffffff)<<16|(p[3]&0xffffffff)>>16;
+  //std::cout << "e0,e1 = " << std::hex << e0 << "," << e1 << std::endl;
+  
+  v[ 0]=(e0>> 1)&0x7f;
+  v[ 1]=(e0>> 8)&0x7f;
+  v[ 2]=(e0>>15)&0x7f;
+  v[ 3]=(e0>>22)&0x7f;
+  v[ 4]=(e0>>29)&0x7f;
+  
+  v[ 5]=(e1    )&0x7f;
+  v[ 6]=(e1>> 7)&0x7f;
+  v[ 7]=(e1>>14)&0x7f;
+  v[ 8]=(e1>>21)&0x7f;
+  v[ 9]=(e1>>28)&0x7f;
+  v[10]=(e1>>35)&0x7f;
+  v[11]=(e1>>42)&0x7f;
+  
+  e0= p[0]&0xffffffff00000000     |(p[1]&0xffffffff00000000)>>32;
+  e1=(p[1]&0xffffffff00000000)<<16|(p[2]&0xffffffff00000000)>>16|(p[3]&0xffffffff00000000)>>48;
+  //std::cout << "e0,e1 = " << std::hex << e0 << "," << e1 << std::endl;
+  
+  v[12]=(e0>> 1)&0x7f;
+  v[13]=(e0>> 8)&0x7f;
+  v[14]=(e0>>15)&0x7f;
+  v[15]=(e0>>22)&0x7f;
+  v[16]=(e0>>29)&0x7f;
+  
+  v[17]=(e1    )&0x7f;
+  v[18]=(e1>> 7)&0x7f;
+  v[19]=(e1>>14)&0x7f;
+  v[20]=(e1>>21)&0x7f;
+  v[21]=(e1>>28)&0x7f;
+  v[22]=(e1>>35)&0x7f;
+  v[23]=(e1>>42)&0x7f;
+}
+
+void unpackerEnergies(const uint64_t *p, std::vector<uint16_t> &v) {
+  v.resize(24);
+
+  for(unsigned i(0);i<6;i++) {
+    v[2*i   ]=(p[i]    )&0x7f;
+    v[2*i+ 1]=(p[i]>>13)&0x7f;
+    v[2*i+12]=(p[i]>>32)&0x7f;
+    v[2*i+13]=(p[i]>>45)&0x7f;
+  }
+}
 
 int main(int argc, char** argv) {
   if(argc<2) {
@@ -89,6 +143,33 @@ int main(int argc, char** argv) {
 	std::cout << std::endl;
 	rEvent->print();
 	const uint64_t *p64(((const uint64_t*)rEvent)+1);
+
+	std::vector<uint16_t> v;
+	for(unsigned u(0);u<15;u++) {
+	  econtEnergies(p64+4+4*u,v);
+	  std::cout << "ECON-T energies " << u << std::endl;
+	  for(unsigned j(0);j<12;j++) {
+	    std::cout << " " << std::setw(3) << v[j];
+	  }
+	  std::cout << std::endl;
+	  for(unsigned j(12);j<24;j++) {
+	    std::cout << " " << std::setw(3) << v[j];
+	  }
+	  std::cout << std::endl;
+	}
+	for(unsigned u(0);u<7;u++) {
+	  unpackerEnergies(p64+65+6*u,v);
+	  std::cout << "Unpacker energies " << u << std::endl;
+	  for(unsigned j(0);j<12;j++) {
+	    std::cout << " " << std::setw(3) << v[j];
+	  }
+	  std::cout << std::endl;
+	  for(unsigned j(12);j<24;j++) {
+	    std::cout << " " << std::setw(3) << v[j];
+	  }
+	  std::cout << std::endl;
+	}
+	
 	/*
 	b->print();
 	e->print();
